4b45852d30add12360e71ddc0e65e5c44b7ae526
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2020-07-09 16:04:18 +0530

    Updates

---
+++ precache-manifest.0cd12831be150a9a8491f5fcccd0c47a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435cfecca719665f5817ba7c9909d337",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "6763433641a77ae3532f",
+    "url": "/monsters-rolodex-react/static/css/main.a5d3ca02.chunk.css"
+  },
+  {
+    "revision": "082f8914de693280636e",
+    "url": "/monsters-rolodex-react/static/js/2.9cbb02df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.9cbb02df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6763433641a77ae3532f",
+    "url": "/monsters-rolodex-react/static/js/main.56cc1bfb.chunk.js"
+  },
+  {
+    "revision": "c922bb9e0f242a171c2c",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.3e6eb4a3.js"
+  }
+]);
